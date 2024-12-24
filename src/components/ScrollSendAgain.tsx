7aed1ebe9d130376e6c68d7b93f210724d4d767a
santiago <frontend@arcoch.com>
2024-12-23 23:39:43 -0300

    Add: scrollView

--- src/components/ScrollSendAgain.tsx
+++ src/components/ScrollSendAgain.tsx
@@ -1,31 +1,44 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import {ScrollView} from 'react-native-gesture-handler';
 
+interface ItemsProps {
+  id: string | number;
+  image: any;
+  label: string;
+}
+
 export default function ScrollSendAgain() {
-  const items = [
-    {id: 1, title: 'Item 1', amount: '$100'},
-    {id: 2, title: 'Item 2', amount: '$200'},
-    {id: 3, title: 'Item 3', amount: '$300'},
-    {id: 4, title: 'Item 4', amount: '$400'},
-    {id: 5, title: 'Item 5', amount: '$500'},
+  const items: ItemsProps[] = [
+    {id: 1, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 2, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 3, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 4, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 5, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 6, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 7, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 8, image: require('../assets/icons/home.png'), label: 'person'},
+    {id: 9, image: require('../assets/icons/home.png'), label: 'person'},
   ];
 
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
-      className="px-4">
+      contentContainerStyle={{gap: 12}}
+      className="pt-4">
       {items.map(item => (
         <TouchableOpacity
           key={item.id}
-          className="bg-white mr-4 rounded-xl p-4 shadow-sm w-40">
-          <View className="space-y-2">
-            <Text className="text-purple-600 font-bold text-lg">
-              {item.title}
-            </Text>
-            <Text className="text-gray-600 text-base">{item.amount}</Text>
-          </View>
+          className="rounded-xl w-20 h-24 items-center justify-center ">
+          <Image
+            source={item.image}
+            style={{width: 65, height: 65}}
+            resizeMode="contain"
+          />
+          <Text className="text-gray-600 text-center text-base">
+            {item.label}
+          </Text>
         </TouchableOpacity>
       ))}
     </ScrollView>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import ScrollSendAgain from '../components/ScrollSendAgain';
+import person from '../assets/icons/home.png';
+import {useNavigation} from '@react-navigation/native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate('Profile');
+  };
   return (
     <SafeAreaProvider>
       <View className="bg-[#0FD08B] h-full">
-        <View className="h-1/4">
-          <Text className="text-red-500">Home Screen</Text>
+        <View className="h-1/4 w-full">
+          <TouchableOpacity
+            className="flex-row items-center py-4 pl-3 gap-2"
+            onPress={handlePress}>
+            <Image
+              source={person}
+              style={{width: 32, height: 32}}
+              resizeMode="contain"
+            />
+            <Text className="text-white font-bold text-[15px]">Juan Perez</Text>
+          </TouchableOpacity>
+          <View className="w-full pt-4">
+            <Text className="text-white text-center text-[16px]">
+              Your Balance
+            </Text>
+            <Text className="text-white text-center font-bold text-[32px]">
+              $ 2.800
+            </Text>
+          </View>
         </View>
-        <View className="bg-white h-3/4 rounded-t-[3rem] px-3 pt-5">
-          <Text className="text-[#121212] text-2xl font-bold">Send Again</Text>
+        <View className="bg-white h-3/4 rounded-t-[3rem] px-3 pt-6">
+          <Text className="text-[#121212] text-center text-2xl font-bold">
+            Send Again
+          </Text>
           <ScrollSendAgain />
         </View>
       </View>
